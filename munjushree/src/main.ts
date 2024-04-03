43e9c181c70f1aea6e9c0eea19caddb05f7a4cee
sairash <gautamsairash@gmail.com>
2024-04-04 01:04:16 +0545

    vant for better ui

--- munjushree/src/main.ts
+++ munjushree/src/main.ts
@@ -1,8 +1,11 @@
-import './assets/main.css'
+import 'vant/lib/index.css';
 import 'leaflet/dist/leaflet.css';
+import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
+import { Toast, FloatingPanel, Form, Field, CellGroup, Button, Overlay, Notify } from 'vant';
 
 import App from './App.vue'
 import router from './router'
@@ -11,5 +14,7 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.use(Toast).use(FloatingPanel).use(Form).use(Field).use(CellGroup).use(Button).use(Overlay).use(Notify)
 
 app.mount('#app')
