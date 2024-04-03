ff8eeea50a2b653422f6dd3c8bb749c7c9974b39
sairash <gautamsairash@gmail.com>
2024-04-04 01:03:15 +0545

    localstorage save name

--- munjushree/src/stores/counter.ts
+++ munjushree/src/stores/counter.ts
@@ -1,11 +1,41 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
+import { Preferences } from '@capacitor/preferences';
+
+
 
 export const useInfoStore = defineStore('info', () => {
-  const name = ref("")
-  function set_name(value: string) {
-    name.value = value
+  async function set_name(value: string) {
+    await Preferences.set({
+      key: 'name',
+      value: value
+    });
   }
 
-  return { name, set_name }
+  async function set_color(value: string) {
+    await Preferences.set({
+      key: 'color',
+      value: value
+    });
+  }
+
+  async function get_name():Promise<string> {
+    const name = (await Preferences.get({
+      key: 'name',
+    })).value as string;
+
+    return name == null? "": name;
+  }
+
+  async function get_color():Promise<number> {
+    const name = parseInt((await Preferences.get({
+      key: 'color',
+    })).value as string);
+
+    return Number.isNaN(name)?0:name;
+  }
+
+
+
+  return { set_name, set_color, get_name, get_color }
 })
